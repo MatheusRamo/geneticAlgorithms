b10c904914383f8cee2405b106eae0580db2c7cf
matheus <geof.matheus@gmail.com>
2022-03-01 01:37:35 -0300

    add genetic algorithm

---
+++ genetic_algorithm.py
@@ -0,0 +1,10 @@
+class Product():
+    def __init__(self, name, space, price):
+        self.name = name
+        self.space = space
+        self.price = price
+
+
+
+if __name__=='__main__':
+    print("Loding Genetic Algorithm")
